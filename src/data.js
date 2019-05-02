1feeef29eec9d8ec45720a4bc2d85f4bb40cc789
JenSarela <jen.sarela24@gmail.com>
2019-05-01 21:53:02 -0400

    trabajando en Dom

--- src/data.js
+++ src/data.js
@@ -3,74 +3,70 @@
 // puedes ver como agregamos la función a nuestro objeto global window 
 //declaramos variable 
 
+//llamando de la forma cipher pero para filtrar
+//window.dexPoke = {
+const pokedexType = (codePoke, condicion) => {
+  const pokeByFilter = codePoke.filter(element => {
 
-const filterType = (codePoke, condition) => {
-  /* Acá va tu código */
-  let filterType = codePoke.filter(element => {
+    return element.type.includes(condicion);
+    //let filterType = codePoke.filter(element => {
     //el filter pasa por todos los elementos del array
     //element va a tomar ese valor en cada vuelta
-    return element.type.includes(condition);
+    //return element.type.includes(condition);
   });
-  return resultFilterType;
+
+  return pokeByFilter;
+
 }
-window.filterType = filterType;
-window.win.onload=function getAllPokemon()
-{
-	var pokemonList = POKEMON.pokemon;
-	return(pokemonList);	
-}
-function getPokemonByName(name)
-{	
-	var pokemonList = POKEMON.pokemon;//pokrmonlist es la variable que guardara el pokemon que se busque
-	for(var i = 0; i < pokemonList.length;i++)
-	{
-		if(pokemonList[i].name.toLowerCase() == name.toLowerCase())
-			return(pokemonList[i]);
-	}
-	return("No existe ese Pokemon");
+window.pokedexType = pokedexType;
+
+
+
+
+function getPokemonByName(name) {
+  var pokemonList = POKEMON.pokemon; //pokrmonlist es la variable que guardara el pokemon que se busque
+  for (var i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].name.toLowerCase() == name.toLowerCase())
+      return (pokemonList[i]);
+  }
+  return ("No existe ese Pokemon");
 }
 
 
-function getPokemonById(id)
-{
+function getPokemonById(id) {
 
-	var pokemonList = POKEMON.pokemon;
-	for(var i = 0; i < pokemonList.length;i++)
-	{
-		if(pokemonList[i].id == id)
-			return(pokemonList[i]);
-	}
-	return("No existe ese Pokemon");
+  var pokemonList = POKEMON.pokemon;
+  for (var i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].id == id)
+      return (pokemonList[i]);
+  }
+  return ("No existe ese Pokemon");
 }
-function getPokemonByType(type)
-{
-	var pokemonList = POKEMON.pokemon;
-	var foundList = [];
-	for(var i = 0; i < pokemonList.length;i++)
-	{
-		for(var j = 0; j< pokemonList[i].type.length; j++)
-		{
-			if(pokemonList[i].type[j].toLowerCase() == type.toLowerCase())
-				foundList.push(pokemonList[i]);
-		}
-	}
-	if(foundList.length > 0)
-		return(foundList);
-	return("No existen pokemones asociados a ese tipo");
-}
-function getPokemonByWeakness(weaknesses)
-{
-	var pokemonList = POKEMON.pokemon;
-	var foundList = [];
-	for(var i = 0; i < pokemonList.length;i++)
-	{
-		for(var j = 0; j< pokemonList[i].weaknesses.length; j++)
-		{
-			if(pokemonList[i].weaknesses[j].toLowerCase() == weaknesses.toLowerCase())
-				foundList.push(pokemonList[i]);
-		}
-	}
-	if(foundList.length > 0)
-		return(foundList);
-	return("No existen pokemones asociados a esa debilidad");
 
+function getPokemonByType(type) {
+  var pokemonList = POKEMON.pokemon;
+  var foundList = [];
+  for (var i = 0; i < pokemonList.length; i++) {
+    for (var j = 0; j < pokemonList[i].type.length; j++) {
+      if (pokemonList[i].type[j].toLowerCase() == type.toLowerCase())
+        foundList.push(pokemonList[i]);
+    }
+  }
+  if (foundList.length > 0)
+    return (foundList);
+  return ("No existen pokemones asociados a ese tipo");
+}
+
+function getPokemonByWeakness(weaknesses) {
+  var pokemonList = POKEMON.pokemon;
+  var foundList = [];
+  for (var i = 0; i < pokemonList.length; i++) {
+    for (var j = 0; j < pokemonList[i].weaknesses.length; j++) {
+      if (pokemonList[i].weaknesses[j].toLowerCase() == weaknesses.toLowerCase())
+        foundList.push(pokemonList[i]);
+    }
+    if (foundList.length > 0)
+      return (foundList);
+	return ("No existen pokemones asociados a esa debilidad");
+};
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 //declaramos una variable para llamar la data 
 let codePoke = POKEMON.pokemon;
 //aqui estamos mostrando todos los pokemon  
+const root = document.getElementById("root");
+
+let filtType = document.getElementById("seleccion");
 //console.log(codePoke);
 //mostrar toda la data a traves de una variable, tomando un id del body
 let rootBody = document.getElementById("root");
@@ -27,29 +30,22 @@ let pokepoke = (codePoke) => {
 window.onload = pokepoke(codePoke);
 
 let selectType = "";
-let filter =codePoke.forEach(element => {
-
-  
-});
-
-selectType = (codePoke, condition) => {
- let selectTypeResult = codePoke.filter( element => {
-   return element.type == "condition"
- })
-}
-//window.selectType = selectType;
-// al final declaramos todos lo window.(....) que sean necesarios para hacer globales todas las funciones
+window.selectType = selectType;
 let filType = document.getElementById("type-select");
 let guardar = document.getElementById("root");
 filType.addEventListener("change", () => {
+		//window.dexPoke.filterType(codePoke, element.getAttribute("poke-type"));
+	//});
 
 let condition = filType.options
 [filType.selectedIndex].text;
 console.log(condition);
+});
 
-let getPoke= "";
-for(let i = 0; i <codePoke.length; i++) {
-  `<div class="col-sm-2">
+let filter =codePoke.filter(element => {
+  for(let i= 0; i <selectType.length; i++){
+    rootBody.innerHTML =[];
+    `<div class="col-sm-2">
       <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
       <div class="card-body">
           <div class="col-sm-2">
@@ -60,7 +56,50 @@ for(let i = 0; i <codePoke.length; i++) {
         </div>
       </div>
     </div>`
+  }
+console.log(codePoke.type);  
+});
+/*window.onload= filter(codePoke.filter);
+selectType = (codePoke, condition) => {
+ let selectTypeResult = codePoke.filter( element => {
+   return element.type == "condition"
+ })
+}*/
+
+//llamando a funcion de filtrado
+
+/*Filtrar pokemones*/
+
+filtType.addEventListener("change",()=>{
+   rootBody = '';
+    let condicion = document.getElementById("filtType");
+        
+    const pokeByFilter = window.pokedexType(codePoke, condicion);
+    for ( let i=0; i<pokeByFilter.length;i++){
+        result += `<div class="col-sm-2">
+        <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
+        <div class="card-body">
+            <div class="col-sm-2">
+          <h5 class="card-title">"Nombre:${codePoke[i].name}"</h5>
+          <h6 class="card-title">"Número:${codePoke[i].num}"</h6>
+          <p class="card-text">"Tipo:${codePoke[i].type}"</p>
+          <a href="#" class="btn-primary">Evolucion</a>
+          </div>
+        </div>
+      </div>`
+        for ( let pikachu=0; pikachu<pokeByFilter[i].type.length;pikachu++){
+        
+            result += `<div id="result">
+          ${pokeByFilter[i].type[pikachu]} </div>`
+          
+        }
+     
+
+
+    
+
+    rootBody.innerHTML = result;
+
 }
 
 });
-
