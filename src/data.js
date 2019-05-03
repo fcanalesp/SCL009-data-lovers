d2b0860a4d455e61b035762cc929dcd0f2a78366
fcanalesp <fccp@protonmail.com>
2019-05-03 09:56:38 -0400

    filter filtrando

--- src/data.js
+++ src/data.js
@@ -1,72 +1,16 @@
-/* Manejo de data */
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window 
-//declaramos variable 
+//esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
 
-//llamando de la forma cipher pero para filtrar
-//window.dexPoke = {
-  const pokedexType = (codePoke, condicion) => {
-    const pokeByFilter = codePoke.filter(element => {
-  
-      return element.type.includes(condicion);
-      //let filterType = codePoke.filter(element => {
-      //el filter pasa por todos los elementos del array
-      //element va a tomar ese valor en cada vuelta
-      //return element.type.includes(condition);
-    });
-  
-    return pokeByFilter;
-  
-  }
-  window.pokedexType = pokedexType;
-  
-  
-  
-  
-  function getPokemonByName(name) {
-    var pokemonList = POKEMON.pokemon; //pokrmonlist es la variable que guardara el pokemon que se busque
-    for (var i = 0; i < pokemonList.length; i++) {
-      if (pokemonList[i].name.toLowerCase() == name.toLowerCase())
-        return (pokemonList[i]);
-    }
-    return ("No existe ese Pokemon");
-  }
-  
-  
-  function getPokemonById(id) {
-  
-    var pokemonList = POKEMON.pokemon;
-    for (var i = 0; i < pokemonList.length; i++) {
-      if (pokemonList[i].id == id)
-        return (pokemonList[i]);
-    }
-    return ("No existe ese Pokemon");
-  }
-  
-  function getPokemonByType(type) {
-    var pokemonList = POKEMON.pokemon;
-    var foundList = [];
-    for (var i = 0; i < pokemonList.length; i++) {
-      for (var j = 0; j < pokemonList[i].type.length; j++) {
-        if (pokemonList[i].type[j].toLowerCase() == type.toLowerCase())
-          foundList.push(pokemonList[i]);
-      }
-    }
-    if (foundList.length > 0)
-      return (foundList);
-    return ("No existen pokemones asociados a ese tipo");
-  }
-  
-  function getPokemonByWeakness(weaknesses) {
-    var pokemonList = POKEMON.pokemon;
-    var foundList = [];
-    for (var i = 0; i < pokemonList.length; i++) {
-      for (var j = 0; j < pokemonList[i].weaknesses.length; j++) {
-        if (pokemonList[i].weaknesses[j].toLowerCase() == weaknesses.toLowerCase())
-          foundList.push(pokemonList[i]);
-      }
-      if (foundList.length > 0)
-        return (foundList);
-    return ("No existen pokemones asociados a esa debilidad");
-  };
-  }
+//FILTRANDO!
+const filterPoke = (codePoke, condition) => {
+  let filterTypeResult = codePoke.filter(element => {
+    
+    return element.type.includes(condition);
+  })
+  return filterTypeResult;
+};
+
+window.filterPoke = filterPoke;
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,98 +1,52 @@
 /* Manejo del DOM */
-//declaramos una variable para llamar la data 
-let codePoke = POKEMON.pokemon;
-//aqui estamos mostrando todos los pokemon  
-const root = document.getElementById("root");
+// MOSTRANDO DATA
+const codePoke = POKEMON.pokemon; //variable para LLAMAR a la data
+const rootBody = document.getElementById("root");// variable para MOSTRAR la data
 
-let filtType = document.getElementById("type");
 
-//console.log(codePoke);
-//mostrar toda la data a traves de una variable, tomando un id del body
-let rootBody = document.getElementById("root");
-//aqui llamamos a los pokemon solo por sus nombres en el orden que esta establecido en la data
-let pokepoke = (codePoke) => {
-  for (let i = 0; i < codePoke.length; i++) {
-    rootBody.innerHTML +=
-      `<div class="col-sm-2">
-      <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
-      <div class="card-body">
-          <div class="col-sm-2">
-        <h5 class="card-title">"Nombre:${codePoke[i].name}"</h5>
-        <h6 class="card-title">"Número:${codePoke[i].num}"</h6>
-        <p class="card-text">"Tipo:${codePoke[i].type}"</p>
-        <a href="#" class="btn-primary">Evolucion</a>
-        </div>
-      </div>
-    </div>`
-    //console.log(codePoke[i].name);
-
-  }
-}
-window.onload = pokepoke(codePoke);
-
-let selectType = "";
-window.selectType = selectType;
-let guardar = document.getElementById("root");
-filtType.addEventListener("change", () => {
-		//window.pokeByFilter.filterType(codePoke, element.getAttribute(""));
-	//});
-
-let condition = filtType.options
-[filtType.selectedIndex].text;
-console.log(condition);
-});
-
-let filter =codePoke.filter(element => {
-  for(let i= 0; i <selectType.length; i++){
-    rootBody.innerHTML =[];
-    `<div class="col-sm-2">
-      <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
-      <div class="card-body">
-          <div class="col-sm-2">
-        <h5 class="card-title">"Nombre:${codePoke[i].name}"</h5>
-        <h6 class="card-title">"Número:${codePoke[i].num}"</h6>
-        <p class="card-text">"Tipo:${codePoke[i].type}"</p>
-        <a href="#" class="btn-primary">Evolucion</a>
-        </div>
-      </div>
-    </div>`
-  }
-console.log(codePoke.type);  
-});
-/*window.onload= filter(codePoke.filter);
-selectType = (codePoke, condition) => {
- let selectTypeResult = codePoke.filter( element => {
-   return element.type == "condition"
- })
-}*/
-
-//llamando a funcion de filtrado
-
-/*Filtrar pokemones*/
-
-filtType.addEventListener("change",()=>{
-   rootBody = '';
-    let condicion = document.getElementById("filtType");
-        
-    const pokeByFilter = window.pokedexType(codePoke, condicion);
-    for ( let i=0; i<pokeByFilter.length;i++){
-        rootBody.innerHTML += `<div class="col-sm-2">
+let pokepoke = (codePoke) => { // LLamando pokemones por el orden establecido por la data.
+    for (let i = 0; i < codePoke.length; i++) {
+        rootBody.innerHTML +=
+            `<div class="col-sm-2">
         <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
         <div class="card-body">
             <div class="col-sm-2">
-          <h5 class="card-title">"Nombre:${codePoke[i].name}"</h5>
-          <h6 class="card-title">"Número:${codePoke[i].num}"</h6>
-          <p class="card-text">"Tipo:${codePoke[i].type}"</p>
+          <h5 class="card-title">Nombre:${codePoke[i].name}</h5>
+          <h6 class="card-title">Número:${codePoke[i].num}</h6>
+          <p class="card-text">Tipo:${codePoke[i].type}</p>
           <a href="#" class="btn-primary">Evolucion</a>
           </div>
         </div>
       </div>`
-        for ( let pikachu=0; pikachu<pokeByFilter[i].type.length;pikachu++){
-        
-            rootBody.innerHTML += `<div id="result">${pokeByFilter[i].type[pikachu]}</div>`
-          
-        }
-
+        //xconsole.log(codePoke[i].name);
+    }
 }
 
+window.onload = pokepoke(codePoke);// para que cada vez que se cargue la pagina, se muestre toda la data
+
+//filtrar
+//let containerRoot = document.getElementById("result");
+let selectType = document.getElementById("type");
+selectType.addEventListener("change", () => {
+  let tipo = selectType.options[selectType.selectedIndex].value;
+  console.log(tipo);
+  
+  let conecta = window.filterPoke(codePoke, tipo);
+  rootBody.innerHTML = "";
+    
+  conecta.forEach(element => {
+    rootBody.innerHTML +=
+    `<div class="col-sm-2">
+        <img src="${element.img}" class="card-img-top" alt="Poke">
+        <div class="card-body">
+            <div class="col-sm-2">
+          <h5 class="card-title">Nombre:${element.name}</h5>
+          <h6 class="card-title">Número:${element.num}</h6>
+          <p class="card-text">Tipo:${element.type}</p>
+          <a href="#" class="btn-primary">Evolucion</a>
+          </div>
+        </div>
+      </div>`
+  });
 });
+
