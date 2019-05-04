1d0ca53e11f5184b1e6f830c77e20db66f82d366
fcanalesp <fccp@protonmail.com>
2019-05-04 13:58:39 -0400

    filtrar por debilidad listo, pero su test no corre por completo

--- src/main.js
+++ src/main.js
@@ -1,23 +1,4 @@
 /* Manejo del DOM */
-<<<<<<< HEAD
-// MOSTRANDO DATA
-const codePoke = POKEMON.pokemon; //variable para LLAMAR a la data
-const rootBody = document.getElementById("root");// variable para MOSTRAR la data
-
-
-let pokepoke = (codePoke) => { // LLamando pokemones por el orden establecido por la data.
-    for (let i = 0; i < codePoke.length; i++) {
-        rootBody.innerHTML +=
-            `<div class="col-sm-2">
-        <img src="${codePoke[i].img}" class="card-img-top" alt="Poke">
-        <div class="card-body">
-            <div class="col-sm-2">
-          <h5 class="card-title">Nombre:${codePoke[i].name}</h5>
-          <h6 class="card-title">Número:${codePoke[i].num}</h6>
-          <p class="card-text">Tipo:${codePoke[i].type}</p>
-          <a href="#" class="btn-primary">Evolucion</a>
-          </div>
-=======
 //declaramos una variable para llamar la data 
 const codePoke = window.POKEMON.pokemon;
 //aqui estamos mostrando todos los pokemon  
@@ -40,50 +21,18 @@ let pokepoke = (codePoke) => {
         <h6 class="card-title">"Número:${codePoke[i].num}"</h6>
         <p class="card-text">"Tipo:${codePoke[i].type}"</p>
         <a href="#" class="btn-primary">Evolucion</a>
->>>>>>> d72b02b7e3fbe9b9abe811bebd4c1bed3a955c58
         </div>
       </div>`
-        //xconsole.log(codePoke[i].name);
+        
     }
 }
-<<<<<<< HEAD
-
-window.onload = pokepoke(codePoke);// para que cada vez que se cargue la pagina, se muestre toda la data
-
-//filtrar
-//let containerRoot = document.getElementById("result");
-let selectType = document.getElementById("type");
-selectType.addEventListener("change", () => {
-  let tipo = selectType.options[selectType.selectedIndex].value;
-  console.log(tipo);
-  
-  let conecta = window.filterPoke(codePoke, tipo);
-  rootBody.innerHTML = "";
-    
-  conecta.forEach(element => {
-    rootBody.innerHTML +=
-    `<div class="col-sm-2">
-        <img src="${element.img}" class="card-img-top" alt="Poke">
-        <div class="card-body">
-            <div class="col-sm-2">
-          <h5 class="card-title">Nombre:${element.name}</h5>
-          <h6 class="card-title">Número:${element.num}</h6>
-          <p class="card-text">Tipo:${element.type}</p>
-          <a href="#" class="btn-primary">Evolucion</a>
-          </div>
-        </div>
-      </div>`
-  });
-});
-
-=======
 window.onload = pokepoke(codePoke);
 
-//window.selectType = selectType;
-let guardar = document.getElementById("type");
-guardar.addEventListener("change", () => {
 
-  let condition = guardar.options[guardar.selectedIndex].value;
+let saveForType = document.getElementById("type");
+saveForType.addEventListener("change", () => {
+
+  let condition = saveForType.options[saveForType.selectedIndex].value;
   //console.log(condition);
   let filter = window.pokedexType(codePoke, condition);
   rootBody.innerHTML = "";
@@ -106,11 +55,37 @@ guardar.addEventListener("change", () => {
 
 });
 
-//llamar funcion ordenar
+// FILTRANDO POR DEBILIDADES
+let saveForWeakness= document.getElementById("weaknesses");
+saveForWeakness.addEventListener("change", () => {
 
+  let conditionTwo = saveForWeakness.options[saveForWeakness.selectedIndex].value;
+  //console.log(condition);
+  let filter = window.pokedexWeaknesses(codePoke, conditionTwo);
+  rootBody.innerHTML = "";
+
+  filter.forEach(element => {
+    rootBody.innerHTML +=
+
+      `<div class="col-sm-2">
+    <img src="${element.img}" class="card-img-top" alt="Poke">
+    <div class="card-body">
+        <div class="col-sm-2">
+      <h5 class="card-title">${element.name}"</h5>
+      <h6 class="card-title">"Número:${element.num}"</h6>
+      <p class="card-text">"Tipo:${element.type}"</p>
+      <a href="#" class="btn-primary">Evolucion</a>
+      </div>
+    </div>
+  </div>`
+  })
+
+});
+
+
+//llamar funcion ordena
 /*let order= document.getElementById("descendente");
 order.addEventListener('change', function (){
 
   let ordenados= window.pokedexType(codePoke,"name",)
 })*/
->>>>>>> d72b02b7e3fbe9b9abe811bebd4c1bed3a955c58
